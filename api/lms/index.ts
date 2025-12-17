69771ecc7575b172a7b827a0b880bc8bded84559
CassiaHelenSrt <cassiahelen9213@gmail.com>
2025-12-17 16:44:15 -0300

    post 1

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,8 +1,42 @@
 import { Api } from "../../core/utils/abstract.ts";
+import { RouteError } from "../../core/utils/route-erro.ts";
 import { lmsTables } from "./tables.ts";
 
 export class LmsApi extends Api {
+    handlers = {
+        postCourses: (req, res) => {
+            const { slug, title, description, lessons, hours } = req.body;
+
+            const writeResult = this.db
+                .query(
+                    /*sql*/ `
+                    INSERT OR IGNORE INTO "courses"
+                    ("slug", "title", "description","lessons", "hours")
+                    VALUES (?, ?, ?, ?, ?)
+                    `
+                )
+                .run(slug, title, description, lessons, hours);
+
+            console.log(writeResult);
+
+            /*quando ja existe este curso criado retorna esse error*/
+            if (writeResult.changes === 0) {
+                throw new RouteError(400, "Curso já existe ou erro ao criar");
+            }
+
+            res.status(201).json({
+                id: writeResult.lastInsertRowid,
+                changes: writeResult.changes,
+                title: "curso criado",
+            });
+        },
+    } satisfies Api["handlers"];
+
     tables(): void {
         this.db.exec(lmsTables);
+    }
+
+    routes(): void {
+        this.router.post("/lms/courses", this.handlers.postCourses);
     }
 }
--- client.js
+++ client.js
@@ -3,11 +3,33 @@ console.clear();
 const base = "http://localhost:3000";
 
 const functions = {
-    async getProduct() {
-        const response = await fetch(base + "/products/notebook");
+    async postCourses() {
+        const response = await fetch(base + "/lms/courses", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+
+            body: JSON.stringify({
+                slug: "html-e-css-2",
+                title: "HTML e CSS",
+                description: "curso de html e css para iniciante",
+                lessons: 40,
+                hours: 10,
+            }),
+        });
+
         const body = await response.json();
         console.table(body);
     },
 };
+const fn = process.argv[2];
 
-functions[process.argv[2]]();
+if (!functions[fn]) {
+    console.error("Função não encontrada:", fn);
+    console.log("Funções disponíveis:", Object.keys(functions));
+    process.exit(1);
+}
+
+functions[fn]();
+// functions[process.argv[2]]();
