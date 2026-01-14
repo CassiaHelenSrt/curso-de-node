5756d9dd6bdbc87ba9c94120aeb306da6e8d88d8
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-01-14 12:40:12 -0300

    Session 2

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { AuthApi } from "./api/auth/index.ts";
 import { LmsApi } from "./api/lms/index.ts";
 import { Core } from "./core/core.ts";
 import { logger } from "./core/middleware/logger.ts";
+import { RouteError } from "./core/utils/route-erro.ts";
 
 const core = new Core();
 core.router.use([logger]);
@@ -19,6 +20,27 @@ core.router.get("/", async (req, res) => {
 
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.status(200).end(index);
+});
+
+
+core.router.get("/segura", async (req, res) => {
+    //esse cookies e o ud do usuario autendicado
+    const cookies = req.headers.cookie?.match(/sid=(\d+)/)?.[1]
+
+    if(!cookies){
+         throw new RouteError(404, "não autenticado")
+    }
+
+    const user = core.db.query(`SELECT "email", "email" From "users" WHERE "id" =?`).get(cookies)
+
+    if(!user){
+            throw new RouteError(404, "usuario nao encontrado")
+        }
+
+    console.log(cookies)
+    res.status(200).json(user);
+    // res.status(200).json(cookies);
+    // res.status(200).json('segura');
 });
 
 core.init();
