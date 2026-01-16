5a498dc6e3e9b1974334da611ed882fbd59c0735
CassiaHelenSrt <cassiahelen9213@gmail.com>
2026-01-16 13:11:31 -0300

    randomBytes

---
+++ api/auth/utils.ts
@@ -0,0 +1,6 @@
+import { randomBytes } from "crypto";
+import { promisify } from "util";
+
+export const  randomBytesAsync =  promisify(randomBytes)
+
+
---
+++ cripto.js
@@ -0,0 +1,17 @@
+import { randomBytes } from "crypto";
+import { promisify } from "util";
+
+const randomBytesAsync =  promisify(randomBytes)
+
+
+const buffer =  await randomBytesAsync(32);
+
+
+const hex = buffer.toString('hex')
+const base64 = buffer.toString('base64')
+const base64url = buffer.toString('base64url')
+
+
+console.log(hex)
+console.log(base64)
+console.log(base64url)
